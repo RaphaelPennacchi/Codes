e80003ae1fc9bf1cb3ffd30ad1b15226e98c0076
Raphael Pennacchi <raphaelpennacchi1997@gmail.com>
2020-01-31 11:27:37 -0300

    First commit

--- Python/Sorts/merge.py
+++ Python/Sorts/merge.py
@@ -3,31 +3,22 @@
 def mergeSort(array):
     if (tam := len(array)) == 1:
         return array
-    l1, l2 = array[:tam//2], array[tam//2:]
-    l1, l2 = mergeSort(l1), mergeSort(l2)
-    return merge(l1, l2)
+    esq, dire = mergeSort(array[:tam//2]), mergeSort(array[tam//2:])
+    return merge(esq, dire)
 
 def merge(array1, array2):
     array3 = []
     while(array1 != [] and array2 != []):
         if array1[0] > array2[0]:
-            array3.append(array2[0])
-            del array2[0]
+            array3.append(array2.pop(0))
         else:
-            array3.append(array1[0])
-            del array1[0]
-    while(array1 != []):
-        array3.append(array1[0])
-        del array1[0]
-    while(array2 != []):
-        array3.append(array2[0])
-        del array2[0]
+            array3.append(array1.pop(0))
+    if array1 != [] or array2 != []:
+        array3.extend(array1 or array2)
     return array3
 
 def main():
     lista = [3,4,1,2,3,1,2,5,6,7,3,92,0,2,1,26,-1,2,4,56,238]
-    lista.sort()
-    print(lista)
     print(mergeSort(lista))
 
 if __name__ == "__main__":
---
+++ Python/Sorts/quicksort.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def quicksort(lista):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
