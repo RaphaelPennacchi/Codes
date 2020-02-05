264c52beefc0ab3bf87a8e8ec7888c4a4639b73b
Raphael Pennacchi <raphaelpennacchi1997@gmail.com>
2020-02-05 11:31:37 -0300

    Linguagens Futuras

---
+++ Python/unionFind.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+class UnionFind(object):
+    def __init__(self, arg):
+        super(UnionFind, self).__init__()
+        self.arg = [] * arg
+
+    def union(p, q):
+        pass
+
+    def connected(p, q):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
